c4ad31dbbe9b058bd0c4251ac458bc174f34fa85
ImSejin <hackeres123@gmail.com>
2020-09-17 14:28:52 +0900

    🧪 Add: test code

---
+++ src/test/java/io/github/imsejin/util/CollectionUtilsTest.java
@@ -0,0 +1,30 @@
+package io.github.imsejin.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CollectionUtilsTest {
+
+    @Test
+    public void toMap() {
+        // given
+        List<String> list = Arrays.asList("A", "B", "C");
+        // when
+        Map<Integer, String> map1 = CollectionUtils.toMap(list);
+        // then
+        map1.forEach((k, v) -> assertThat(v, is(list.get(k))));
+
+        // given
+        Set<String> set = new HashSet<>(Arrays.asList("A", "B", "C"));
+        // when
+        Map<Integer, String> map2 = CollectionUtils.toMap(set);
+        // then
+        map2.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+}
