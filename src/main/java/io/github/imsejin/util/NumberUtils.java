6fb4f2d37d1ece21ddbdeb9da8a252e2446c8b7f
ImSejin <hackeres123@gmail.com>
2020-10-03 23:37:44 +0900

    ✨ Add: `NumberUtils#format(double)`

--- src/main/java/io/github/imsejin/util/NumberUtils.java
+++ src/main/java/io/github/imsejin/util/NumberUtils.java
@@ -1,6 +1,18 @@
 package io.github.imsejin.util;
 
+import java.text.NumberFormat;
+
 public final class NumberUtils {
+
+    private static final NumberFormat formatter = NumberFormat.getInstance();
+
+    static {
+        formatter.setGroupingUsed(false);
+    }
+
+    public static String format(double decimal) {
+        return formatter.format(decimal);
+    }
 
     public static double floor(double amount, int len) {
         return Double.parseDouble(String.format("%." + len + "f", amount));
