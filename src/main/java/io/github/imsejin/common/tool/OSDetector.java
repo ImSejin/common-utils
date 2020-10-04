fcc3882a63e4f242b6b9c66ef873549466820df1
ImSejin <hackeres123@gmail.com>
2020-10-04 21:44:02 +0900

    🚿 Clean up: code

--- src/main/java/io/github/imsejin/common/tool/OSDetector.java
+++ src/main/java/io/github/imsejin/common/tool/OSDetector.java
@@ -7,19 +7,13 @@ import io.github.imsejin.common.constant.OperatingSystem;
  */
 public final class OSDetector {
 
-    private static final String CURRENT_OS = System.getProperty("os.name").toLowerCase();
+    private static final String CURRENT_OS_NAME = System.getProperty("os.name").toLowerCase();
 
     private OSDetector() {
     }
 
     public static OperatingSystem getOS() {
-        for (OperatingSystem os : OperatingSystem.values()) {
-            for (String keyword : os.getKeywords()) {
-                if (CURRENT_OS.contains(keyword)) return os;
-            }
-        }
-
-        return null;
+        return OperatingSystem.of(CURRENT_OS_NAME).orElse(null);
     }
 
 }
