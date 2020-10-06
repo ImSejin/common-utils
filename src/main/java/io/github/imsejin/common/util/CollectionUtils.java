62e90399b1afa77b43b0c351c6a8e671f1f23b9e
ImSejin <hackeres123@gmail.com>
2020-10-06 23:28:47 +0900

    ✨ Add: `CollectionUtils#partition(List, int)`

--- src/main/java/io/github/imsejin/common/util/CollectionUtils.java
+++ src/main/java/io/github/imsejin/common/util/CollectionUtils.java
@@ -1,9 +1,7 @@
 package io.github.imsejin.common.util;
 
 import javax.annotation.Nonnull;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Supplier;
 
 /**
@@ -71,6 +69,36 @@ public final class CollectionUtils {
                 (map, streamValue) -> map.put(map.size(), streamValue),
                 (map, map2) -> {
                 });
+    }
+
+    /**
+     * Returns a list that contains consecutive {@link List#subList(int, int)}s.
+     *
+     * <pre>{@code
+     *     partition([1, 2, 3, 4, 5], 3); // [[1, 2, 3], [4, 5]]
+     *     partition([1, 2, 3, 4, 5], 6); // [[1, 2, 3, 4, 5]]
+     * }</pre>
+     *
+     * @param list origin list
+     * @param size partition size
+     * @param <T>  generic type
+     * @return partitioned lists
+     * @throws IllegalArgumentException if size non-positive
+     */
+    public static <T> List<List<T>> partition(@Nonnull List<T> list, int size) {
+        if (size < 1) throw new IllegalArgumentException("Size of each list must be greater than or equal to 1");
+
+        int quotient = Math.floorDiv(list.size(), size);
+
+        List<List<T>> superList = new ArrayList<>();
+        for (int i = 0; i < quotient; i++) {
+            superList.add(list.subList(i * size, (i + 1) * size));
+        }
+
+        int remainder = Math.floorMod(list.size(), size);
+        if (remainder > 0) superList.add(list.subList(size * quotient, size * quotient + remainder));
+
+        return superList;
     }
 
 }
--- src/test/java/io/github/imsejin/common/util/CollectionUtilsTest.java
+++ src/test/java/io/github/imsejin/common/util/CollectionUtilsTest.java
@@ -1,11 +1,16 @@
 package io.github.imsejin.common.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CollectionUtilsTest {
 
@@ -24,6 +29,33 @@ public class CollectionUtilsTest {
         Map<Integer, String> map2 = CollectionUtils.toMap(set);
         // then
         map2.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 10, 33, 369, 5120, 17_726, 655_362, 8_702_145, 12_345_679})
+    public void partition(int size) {
+        // given
+        List<Integer> integers = IntStream.range(0, 12_345_678).boxed().collect(Collectors.toList());
+
+        // when
+        List<List<Integer>> partition = CollectionUtils.partition(integers, size);
+
+        // then
+        int partitionSize = partition.size();
+        int integerSize = integers.size();
+        boolean modExists = Math.floorMod(integerSize, size) > 0;
+
+        for (int i = 0; i < partitionSize; i++) {
+            List<Integer> list = partition.get(i);
+
+            if (modExists && i == partitionSize - 1) {
+                assertEquals(list.size(), integerSize % size);
+            } else {
+                assertEquals(list.size(), size);
+            }
+        }
+
+        System.out.println("partition(" + 12_345_678 + ", " + size + ").size() == " + partitionSize);
     }
 
 }
