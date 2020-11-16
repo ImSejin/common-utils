508c0a222b6f5d5567eacd88bafaae218e198391
ImSejin <hackeres123@gmail.com>
2020-11-16 17:45:26 +0900

    ✨ Add: `IniUtils#write(File, Map)`, `IniUtils#writeEntries(File, Map)`, `IniUtils#read(File)`

--- src/main/java/io/github/imsejin/common/util/IniUtils.java
+++ src/main/java/io/github/imsejin/common/util/IniUtils.java
@@ -6,9 +6,10 @@ import org.ini4j.Profile.Section;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Ini utilities
@@ -16,6 +17,14 @@ import java.util.Map;
 public final class IniUtils {
 
     private IniUtils() {
+    }
+
+    private static void configure(Ini ini) {
+        Config conf = new Config();
+        conf.setMultiSection(true);
+        conf.setFileEncoding(StandardCharsets.UTF_8);
+
+        ini.setConfig(conf);
     }
 
     public static Map<String, String> readSection(File file, String sectionName) {
@@ -32,16 +41,40 @@ public final class IniUtils {
         return new ArrayList<>(section.values());
     }
 
-    private static Ini read(File file) {
-        Config conf = new Config();
-        conf.setMultiSection(true);
-
+    public static Ini read(File file) {
         try {
             Ini ini = new Ini(file);
-            ini.setConfig(conf);
+            configure(ini);
             ini.load();
 
             return ini;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void write(File file, Map<String, Map<String, Object>> data) {
+        // Converts inner maps to sets of entries.
+        Map<String, Set<Map.Entry<String, Object>>> entriesMap = data.entrySet().stream()
+                .collect(toMap(Map.Entry::getKey, it -> it.getValue().entrySet()));
+
+        writeEntries(file, entriesMap);
+    }
+
+    public static void writeEntries(File file, Map<String, Set<Map.Entry<String, Object>>> data) {
+        try {
+            if (!file.exists()) file.createNewFile();
+
+            Ini ini = new Ini(file);
+            configure(ini);
+
+            data.forEach((sectionName, entries) -> {
+                for (Map.Entry<String, ?> entry : entries) {
+                    ini.put(sectionName, entry.getKey(), entry.getValue());
+                }
+            });
+
+            ini.store();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
