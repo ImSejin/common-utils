dc44618399ba2739dd314baa58d73dc3cf494e2d
ImSejin <hackeres123@gmail.com>
2021-08-12 00:00:37 +0900

    ✅ Update: test code for `ReflectionUtils`

--- src/test/java/io/github/imsejin/common/util/ReflectionUtilsTest.java
+++ src/test/java/io/github/imsejin/common/util/ReflectionUtilsTest.java
@@ -17,10 +17,14 @@
 package io.github.imsejin.common.util;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.time.LocalDateTime;
 import java.util.Random;
 import java.util.UUID;
@@ -28,22 +32,11 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("ReflectionUtils")
 class ReflectionUtilsTest {
 
-    private static class A {
-        private static class AA {
-        }
-
-        private class AB {
-        }
-    }
-
-    private class B {
-        private class BA {
-        }
-    }
-
     @Test
+    @DisplayName("method 'getInheritedFields'")
     void getInheritedFields() throws ClassNotFoundException {
         assertThat(ReflectionUtils.getInheritedFields(Parent.class))
                 .isNotNull()
@@ -70,49 +63,102 @@ class ReflectionUtilsTest {
     }
 
     @Test
+    @DisplayName("method 'getFieldValue'")
     void getFieldValue() throws NoSuchFieldException {
-        // given
+        // given: 1
         @RequiredArgsConstructor
         class Model {
             private final long id;
         }
-
         Model model = new Model(new Random().nextLong());
-        Field field = model.getClass().getDeclaredField("id");
+        Field field0 = model.getClass().getDeclaredField("id");
+        // when: 1
+        Object fieldValue0 = ReflectionUtils.getFieldValue(model, field0);
+        // then: 1
+        assertThat(fieldValue0)
+                .isNotNull().isNotEqualTo(0L).isEqualTo(model.id);
 
-        // when & then
-        assertThat(ReflectionUtils.getFieldValue(model, field))
-                .isNotNull()
-                .isNotEqualTo(0L)
-                .isEqualTo(model.id);
+        // given: 2
+        A.id = new Random().nextLong();
+        Field field1 = A.class.getDeclaredField("id");
+        // when: 2
+        Object fieldValue1 = ReflectionUtils.getFieldValue(null, field1);
+        // then: 2
+        assertThat(fieldValue1)
+                .isNotNull().isNotEqualTo(0L).isEqualTo(A.id);
+        A.id = null;
     }
 
     @Test
+    @DisplayName("method 'setFieldValue'")
     void setFieldValue() throws NoSuchFieldException {
-        // given
+        // given: 1
         class Model {
             private String name;
         }
-
         Model model = new Model();
-        Field field = model.getClass().getDeclaredField("name");
-        assertThat(ReflectionUtils.getFieldValue(model, field)).isNull();
-        String fieldValue = UUID.randomUUID().toString();
+        Field field0 = model.getClass().getDeclaredField("name");
+        assertThat(ReflectionUtils.getFieldValue(model, field0)).isNull();
+        // when: 1
+        ReflectionUtils.setFieldValue(model, field0, UUID.randomUUID().toString());
+        // then: 1
+        assertThat(ReflectionUtils.getFieldValue(model, field0))
+                .isNotNull().isEqualTo(model.name);
 
-        // when
-        ReflectionUtils.setFieldValue(model, field, fieldValue);
-
-        // then
-        assertThat(ReflectionUtils.getFieldValue(model, field))
-                .isNotNull().isEqualTo(fieldValue);
+        // given: 2
+        Field field1 = A.AA.class.getDeclaredField("name");
+        assertThat(ReflectionUtils.getFieldValue(null, field1)).isNull();
+        // when: 2
+        ReflectionUtils.setFieldValue(null, field1, UUID.randomUUID().toString());
+        // then: 2
+        assertThat(ReflectionUtils.getFieldValue(null, field1))
+                .isNotNull().isEqualTo(A.AA.name);
+        A.AA.name = null;
     }
 
-    @Test
-    void instantiate() {
-        Stream.of(A.class, A.AA.class)
-                .forEach(clazz -> assertThat(ReflectionUtils.instantiate(clazz))
-                        .isNotNull()
-                        .isExactlyInstanceOf(clazz));
+    @ParameterizedTest
+    @DisplayName("method 'instantiate'")
+    @ValueSource(classes = {A.class, A.AA.class, A.AB.class, B.class, B.BA.class})
+    void instantiate(Class<?> clazz) {
+        // given
+        Class<?>[] paramTypes = null;
+        Object[] initArgs = null;
+        if (!Modifier.isStatic(clazz.getModifiers())) {
+            paramTypes = new Class[]{clazz.getEnclosingClass()};
+
+            if (clazz == B.BA.class) {
+                initArgs = new Object[]{new B()};
+            } else {
+                Object encloser = getClass() == clazz.getEnclosingClass()
+                        ? this : ReflectionUtils.instantiate(clazz.getEnclosingClass());
+                initArgs = new Object[]{encloser};
+            }
+        }
+
+        // when
+        Object instant = ReflectionUtils.instantiate(clazz, paramTypes, initArgs);
+
+        // then
+        assertThat(instant)
+                .isNotNull()
+                .isExactlyInstanceOf(clazz);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    private static class A {
+        private static Long id;
+        private static class AA {
+            private static String name;
+        }
+
+        private class AB {
+        }
+    }
+
+    private class B {
+        private class BA {
+        }
     }
 
 }
