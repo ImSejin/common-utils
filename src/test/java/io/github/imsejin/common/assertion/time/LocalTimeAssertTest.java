fec68bb68675668af3dcd6c861c1e515ae1497fa
imsejin <hackeres123@gmail.com>
2022-05-04 13:11:34 +0900

    ✅ Update: test code

--- src/test/java/io/github/imsejin/common/assertion/time/LocalTimeAssertTest.java
+++ src/test/java/io/github/imsejin/common/assertion/time/LocalTimeAssertTest.java
@@ -79,15 +79,27 @@ class LocalTimeAssertTest {
     @DisplayName("method 'isBeforeNoon'")
     class IsBeforeNoon {
         @Test
-        @DisplayName("passes, when actual is midnight")
+        @DisplayName("passes, when actual is before than noon")
         void test0() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON.minusNanos(1), LocalTime.MIDNIGHT);
 
+            // expect
+            times.forEach(actual -> assertThatNoException()
+                    .isThrownBy(() -> Asserts.that(actual).isBeforeNoon()));
         }
 
         @Test
-        @DisplayName("throws exception, when actual is equal to other")
+        @DisplayName("throws exception, when actual is equal to noon or after than noon")
         void test1() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON, LocalTime.NOON.plusNanos(1),
+                    LocalTime.MAX, LocalTime.of(12, 0));
 
+            // expect
+            times.forEach(actual -> assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Asserts.that(actual).isBeforeNoon())
+                    .withMessageStartingWith("It is expected to be before than the other, but it isn't."));
         }
     }
 
@@ -97,15 +109,27 @@ class LocalTimeAssertTest {
     @DisplayName("method 'isBeforeOrEqualToNoon'")
     class IsBeforeOrEqualToNoon {
         @Test
-        @DisplayName("passes, when actual is midnight")
+        @DisplayName("passes, when actual is before than noon or equal to noon")
         void test0() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON, LocalTime.NOON.minusNanos(1), LocalTime.MIDNIGHT);
 
+            // expect
+            times.forEach(actual -> assertThatNoException()
+                    .isThrownBy(() -> Asserts.that(actual).isBeforeOrEqualToNoon()));
         }
 
         @Test
-        @DisplayName("throws exception, when actual is equal to other")
+        @DisplayName("throws exception, when actual is after than noon")
         void test1() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON.plusNanos(1),
+                    LocalTime.MAX, LocalTime.of(12, 0, 1));
 
+            // expect
+            times.forEach(actual -> assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Asserts.that(actual).isBeforeOrEqualToNoon())
+                    .withMessageStartingWith("It is expected to be before than or equal to the other, but it isn't."));
         }
     }
 
@@ -115,15 +139,27 @@ class LocalTimeAssertTest {
     @DisplayName("method 'isAfternoon'")
     class IsAfternoon {
         @Test
-        @DisplayName("passes, when actual is midnight")
+        @DisplayName("passes, when actual is afternoon")
         void test0() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON.plusNanos(1),
+                    LocalTime.MAX, LocalTime.of(12, 0, 1));
 
+            // expect
+            times.forEach(actual -> assertThatNoException()
+                    .isThrownBy(() -> Asserts.that(actual).isAfternoon()));
         }
 
         @Test
-        @DisplayName("throws exception, when actual is equal to other")
+        @DisplayName("throws exception, when actual is not afternoon")
         void test1() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON, LocalTime.NOON.minusNanos(1), LocalTime.MIDNIGHT);
 
+            // expect
+            times.forEach(actual -> assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Asserts.that(actual).isAfternoon())
+                    .withMessageStartingWith("It is expected to be after than the other, but it isn't."));
         }
     }
 
@@ -133,15 +169,27 @@ class LocalTimeAssertTest {
     @DisplayName("method 'isAfterOrEqualToNoon'")
     class IsAfterOrEqualToNoon {
         @Test
-        @DisplayName("passes, when actual is midnight")
+        @DisplayName("passes, when actual is equal to afternoon or noon")
         void test0() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON, LocalTime.NOON.plusNanos(1),
+                    LocalTime.MAX, LocalTime.of(12, 0));
 
+            // expect
+            times.forEach(actual -> assertThatNoException()
+                    .isThrownBy(() -> Asserts.that(actual).isAfterOrEqualToNoon()));
         }
 
         @Test
-        @DisplayName("throws exception, when actual is equal to other")
+        @DisplayName("throws exception, when actual is before than noon")
         void test1() {
+            // given
+            List<LocalTime> times = Arrays.asList(LocalTime.NOON.minusNanos(1), LocalTime.MIDNIGHT);
 
+            // expect
+            times.forEach(actual -> assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Asserts.that(actual).isAfterOrEqualToNoon())
+                    .withMessageStartingWith("It is expected to be after than or equal to the other, but it isn't."));
         }
     }
 
