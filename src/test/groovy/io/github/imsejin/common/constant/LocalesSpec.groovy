77c64920faa1bf6ab0d569a59c02801be6cfe329
imsejin <hackeres123@gmail.com>
2022-08-16 09:57:47 +0900

    Add: methods `getLanguages()`, `getCountries()` in `Locales`

---
+++ src/test/groovy/io/github/imsejin/common/constant/LocalesSpec.groovy
@@ -0,0 +1,29 @@
+package io.github.imsejin.common.constant
+
+import spock.lang.Specification
+
+class LocalesSpec extends Specification {
+
+    def "Gets all the languages"() {
+        given:
+        def languages = Locales.languages as List<Locale>
+
+        expect:
+        languages != null
+        languages.size() == 46
+        languages.size() == languages.grep { it.language.length() > 0 && it.country.isEmpty() }.collect().size()
+        languages.size() == languages.unique { it.language }.size()
+    }
+
+    def "Gets all the countries"() {
+        given:
+        def countries = Locales.countries as List<Locale>
+
+        expect:
+        countries != null
+        countries.size() == 107
+        countries.size() == countries.grep { !it.language.isEmpty() && !it.country.isEmpty() }.collect().size()
+        countries.size() == countries.unique({ it.toLanguageTag() }).size()
+    }
+
+}
