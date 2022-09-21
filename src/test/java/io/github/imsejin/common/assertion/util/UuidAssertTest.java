d5be513ce6b7447c413ded36b404bc02c0fd3abf
imsejin <hackeres123@gmail.com>
2022-09-21 14:03:42 +0900

    Add: test case

---
+++ src/test/java/io/github/imsejin/common/assertion/util/UuidAssertTest.java
@@ -0,0 +1,81 @@
+package io.github.imsejin.common.assertion.util;
+
+import io.github.imsejin.common.assertion.Asserts;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@DisplayName("UuidAssert")
+class UuidAssertTest {
+
+    @Nested
+    @DisplayName("method 'isNil'")
+    class IsNil {
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "00000000-0000-0000-0000-000000000000",
+        })
+        @DisplayName("passes, when actual is nil")
+        void test0(UUID actual) {
+            assertThatNoException().isThrownBy(() -> Asserts.that(actual).isNil());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "00000000-0000-0000-0000-000000000001",
+                "00000000-0000-0001-0000-000000000000",
+                "123e4567-e89b-12d3-a456-426614174000",
+                "6a7409ee-d6c0-21cc-8fe9-0800093569b9",
+                "5a7448a5-9d71-3bfa-b66d-12459ddf6e2a",
+                "e2bd8ba5-0c35-4801-83cc-ee15fe76cca1",
+                "7348aa4f-bf4e-5419-a0ac-2dd1ddc522d0",
+        })
+        @DisplayName("throws exception, when actual is not nil")
+        void test1(UUID actual) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Asserts.that(actual).isNil())
+                    .withMessageStartingWith("It is expected to be nil, but it isn't.");
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------------
+
+    @Nested
+    @DisplayName("method 'isNotNil'")
+    class IsNotNil {
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "00000000-0000-0000-0000-000000000001",
+                "00000000-0000-0001-0000-000000000000",
+                "123e4567-e89b-12d3-a456-426614174000",
+                "6a7409ee-d6c0-21cc-8fe9-0800093569b9",
+                "5a7448a5-9d71-3bfa-b66d-12459ddf6e2a",
+                "e2bd8ba5-0c35-4801-83cc-ee15fe76cca1",
+                "7348aa4f-bf4e-5419-a0ac-2dd1ddc522d0",
+        })
+        @DisplayName("passes, when actual is not nil")
+        void test0(UUID actual) {
+            assertThatNoException().isThrownBy(() -> Asserts.that(actual).isNotNil());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "00000000-0000-0000-0000-000000000000",
+        })
+        @DisplayName("throws exception, when actual is nil")
+        void test1(UUID actual) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Asserts.that(actual).isNotNil())
+                    .withMessageStartingWith("It is expected not to be nil, but nil.");
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------------
+
+}
