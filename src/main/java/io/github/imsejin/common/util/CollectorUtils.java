8159e26a693db140a27af350d91361093c67511d
ImSejin <hackeres123@gmail.com>
2022-10-29 00:21:36 +0900

    Update: utility `CollectorUtils`

--- src/main/java/io/github/imsejin/common/util/CollectorUtils.java
+++ src/main/java/io/github/imsejin/common/util/CollectorUtils.java
@@ -74,21 +74,79 @@ public final class CollectorUtils {
                             ranks.put(lastRank + items.size(), entry.getValue());
                         },
                         (left, right) -> {
-                            int lastRank = left.lastKey();
-                            List<T> leftItems = left.get(lastRank);
-
-                            Integer firstKey = right.firstKey();
-                            List<T> rightItems = right.get(firstKey);
+                            List<T> leftItems = left.get(left.lastKey());
+                            List<T> rightItems = right.get(right.firstKey());
 
                             R leftProp = propertyExtractor.apply(leftItems.get(0));
                             R rightProp = propertyExtractor.apply(rightItems.get(0));
 
                             if (leftProp == rightProp) {
                                 leftItems.addAll(rightItems);
-                                right.remove(firstKey);
+                                right.remove(right.firstKey());
                             }
 
-                            int offset = lastRank + leftItems.size() - 1;
+                            int offset = left.lastKey() + leftItems.size() - 1;
+                            right.forEach((rank, items) -> left.put(offset + rank, items));
+                        }
+                )
+        );
+    }
+
+    /**
+     * Returns a collector which compares property of each element with extractor and sorts by their dense rank.
+     *
+     * @param <T> type of input element
+     * @return ranking map
+     */
+    public static <T extends Comparable<T>> Collector<T, ?, SortedMap<Integer, List<T>>> denseRanking() {
+        return denseRanking(Function.identity(), T::compareTo);
+    }
+
+    /**
+     * Returns a collector which compares property of each element with extractor and sorts by their dense rank.
+     *
+     * @param propertyExtractor  extractor to get property of the element
+     * @param propertyComparator comparator to the property for ranking
+     * @param <T>                type of input element
+     * @param <R>                type of extracted property from the element
+     * @return ranking map
+     */
+    public static <T, R> Collector<T, ?, SortedMap<Integer, List<T>>> denseRanking(
+            Function<T, R> propertyExtractor, Comparator<R> propertyComparator) {
+        class RankMap extends TreeMap<Integer, List<T>> {
+            @Override
+            public String toString() {
+                return this.entrySet().stream()
+                        .flatMap(e -> e.getValue().stream().map(v -> e.getKey() + " - " + v + " (" + propertyExtractor.apply(v) + ")"))
+                        .collect(joining("\n"));
+            }
+        }
+
+        return collectingAndThen(
+                groupingBy(propertyExtractor, () -> new TreeMap<>(propertyComparator), toList()),
+                map -> map.entrySet().stream().collect(
+                        RankMap::new,
+                        (ranks, entry) -> {
+                            if (ranks.isEmpty()) {
+                                ranks.put(1, entry.getValue());
+                                return;
+                            }
+
+                            ranks.put(ranks.lastKey() + 1, entry.getValue());
+                        },
+                        (left, right) -> {
+                            List<T> rightItems = right.get(right.firstKey());
+                            List<T> leftItems = left.get(left.lastKey());
+
+                            R rightProp = propertyExtractor.apply(rightItems.get(0));
+                            R leftProp = propertyExtractor.apply(leftItems.get(0));
+
+                            if (leftProp == rightProp) {
+                                leftItems.addAll(rightItems);
+                                right.remove(right.firstKey());
+                            }
+
+                            int offset = left.lastKey() - 1;
                             right.forEach((rank, items) -> left.put(offset + rank, items));
                         }
                 )
--- src/test/groovy/io/github/imsejin/common/util/CollectorUtilsSpec.groovy
+++ src/test/groovy/io/github/imsejin/common/util/CollectorUtilsSpec.groovy
@@ -9,6 +9,7 @@ import java.util.Map.Entry
 import java.util.concurrent.TimeUnit
 import java.util.function.Function
 
+import static io.github.imsejin.common.util.CollectorUtils.denseRanking
 import static io.github.imsejin.common.util.CollectorUtils.ranking
 import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.B
 import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.C
@@ -39,7 +40,7 @@ class CollectorUtilsSpec extends Specification {
         def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
         keys == keys.sort(false, Integer::compare)
 
-        then: "it skips rank order as many as the number of previous rank items."
+        then: "It skips rank order as many as the number of previous rank items."
         def rank = 1
         keys.each { assert it == rank; rank += rankMap[it].size() }
 
@@ -68,7 +69,7 @@ class CollectorUtilsSpec extends Specification {
         def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
         keys == keys.sort(false, Integer::compare)
 
-        then: "it skips rank order as many as the number of previous rank items."
+        then: "It skips rank order as many as the number of previous rank items."
         def rank = 1
         keys.each { assert it == rank; rank += rankMap[it].size() }
 
@@ -82,6 +83,65 @@ class CollectorUtilsSpec extends Specification {
         ["beta", "b", "alpha", "a", "gamma", "g", ""] | Function.identity()        | String::compareTo || [1: [""], 2: ["a"], 3: ["alpha"], 4: ["b"], 5: ["beta"], 6: ["g"], 7: ["gamma"]]
         ["beta", "b", "alpha", "a", "gamma", "g", ""] | String::length             | Integer::compare  || [1: [""], 2: ["b", "a", "g"], 5: ["beta"], 6: ["alpha", "gamma"]]
         [C, O, M, M, O, N, U, T, I, L, I, T, I, E, S] | Enum::ordinal              | Integer::compare  || [1: [C], 2: [E], 3: [I, I, I], 6: [L], 7: [M, M], 9: [N], 10: [O, O], 12: [S], 13: [T, T], 15: [U]]
+    }
+
+    @SuppressWarnings("GroovyAssignabilityCheck")
+    def "Collects by dense rank"() {
+        when:
+        def rankMap = values.stream().collect(denseRanking())
+
+        then: "It can't have more keys than the number of origin values."
+        rankMap.size() <= values.size()
+
+        then: "Is it sorted by rank?"
+        def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
+        keys == keys.sort(false, Integer::compare)
+
+        then: "Its rank increments sequentially."
+        def rank = 1
+        keys.each { assert it == rank; rank += 1 }
+        rankMap.values().flatten().unique().size() == keys.size()
+
+        then: "Is it equal to the expected?"
+        rankMap == expected
+
+        where:
+        values                                                          | expected
+        [true, false, false, true]                                      | [1: [false, false], 2: [true, true]]
+        [4, 3, 2, 2, 1, 1, 2]                                           | [1: [1, 1], 2: [2, 2, 2], 3: [3], 4: [4]]
+        [3.14, -1.173, 0.1, -0.2, 0.0]                                  | [1: [-1.173], 2: [-0.2], 3: [0.0], 4: [0.1], 5: [3.14]]
+        [16, 0, -8, 1024, -128, 64, 256, -512, 32]                      | [1: [-512], 2: [-128], 3: [-8], 4: [0], 5: [16], 6: [32], 7: [64], 8: [256], 9: [1024]]
+        ['A', 'b', '9', '0', 'D', 'e', '5'] as List<Character>          | [1: ['0'], 2: ['5'], 3: ['9'], 4: ['A'], 5: ['D'], 6: ['b'], 7: ['e']] as Map<Integer, Character>
+        ["beta", "b", "alpha", "a", "gamma", "g", ""]                   | [1: [""], 2: ["a"], 3: ["alpha"], 4: ["b"], 5: ["beta"], 6: ["g"], 7: ["gamma"]]
+        [I, O, G, I, T, H, U, B, I, M, S, E, J, I, N, C, O, M, M, O, N] | [1: [B], 2: [C], 3: [E], 4: [G], 5: [H], 6: [I, I, I, I], 7: [J], 8: [M, M, M], 9: [N, N], 10: [O, O, O], 11: [S], 12: [T], 13: [U]]
+    }
+
+    def "Collects by dense rank with comparator"() {
+        when:
+        def rankMap = values.stream().collect(denseRanking(extractor as Function, comparator as Comparator)) as SortedMap<Integer, List<?>>
+
+        then: "It can't have more keys than the number of origin values."
+        rankMap.size() <= values.size()
+
+        then: "Is it sorted by rank?"
+        def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
+        keys == keys.sort(false, Integer::compare)
+
+        then: "Its rank increments sequentially."
+        def rank = 1
+        keys.each { assert it == rank; rank += 1 }
+        rankMap.values().stream().flatMap({ it.stream().map(extractor as Function) }).distinct().count() == keys.size()
+
+        then: "Is it equal to the expected?"
+        rankMap == expected
+
+        where:
+        values                                         | extractor                  | comparator        || expected
+        [true, false, false, true]                     | { it.toString().length() } | Integer::compare  || [1: [true, true], 2: [false, false]]
+        [16, 0, -8, 1024, -128, 64, 256, -512, 32]     | Function.identity()        | Integer::compare  || [1: [-512], 2: [-128], 3: [-8], 4: [0], 5: [16], 6: [32], 7: [64], 8: [256], 9: [1024]]
+        ["beta", "b", "alpha", "a", "gamma", "g", ""]  | Function.identity()        | String::compareTo || [1: [""], 2: ["a"], 3: ["alpha"], 4: ["b"], 5: ["beta"], 6: ["g"], 7: ["gamma"]]
+        ["beta", "alpha", "gamma", "delta", "epsilon"] | String::length             | Integer::compare  || [1: ["beta"], 2: ["alpha", "gamma", "delta"], 3: ["epsilon"]]
+        [C, O, M, M, O, N, U, T, I, L, I, T, I, E, S]  | Enum::ordinal              | Integer::compare  || [1: [C], 2: [E], 3: [I, I, I], 4: [L], 5: [M, M], 6: [N], 7: [O, O], 8: [S], 9: [T, T], 10: [U]]
     }
 
     @Timeout(value = 10, unit = TimeUnit.SECONDS)
@@ -107,7 +167,7 @@ class CollectorUtilsSpec extends Specification {
         def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
         keys == keys.sort(false, Integer::compare)
 
-        then: "it skips rank order as many as the number of previous rank items."
+        then: "It skips rank order as many as the number of previous rank items."
         def rank = 1
         keys.each { assert it == rank; rank += rankMap[it].size() }
     }
