2e6bfe37ab6d14508880e2744d596d54b262cb19
imsejin <hackeres123@gmail.com>
2022-10-28 13:46:46 +0900

    Update: utility `CollectorUtils`

--- src/main/java/io/github/imsejin/common/util/CollectorUtils.java
+++ src/main/java/io/github/imsejin/common/util/CollectorUtils.java
@@ -2,21 +2,18 @@ package io.github.imsejin.common.util;
 
 import io.github.imsejin.common.annotation.ExcludeFromGeneratedJacocoReport;
 
-import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collector;
-import java.util.stream.Collector.Characteristics;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Collector utilities
@@ -31,179 +28,69 @@ public final class CollectorUtils {
         throw new UnsupportedOperationException(getClass().getName() + " is not allowed to instantiate");
     }
 
-    public static <T extends Comparable<T>> Collector<T, ?, List<Entry<Integer, T>>> ranking() {
-        return ranking(T::compareTo);
+    /**
+     * Returns a collector which compares property of each element with extractor and sorts by their rank.
+     *
+     * @param <T> type of input element
+     * @return ranking map
+     */
+    public static <T extends Comparable<T>> Collector<T, ?, SortedMap<Integer, List<T>>> ranking() {
+        return ranking(Function.identity(), T::compareTo);
     }
 
-    public static <T> Collector<T, ?, List<Entry<Integer, T>>> ranking(Comparator<? super T> comparator) {
-        return new CollectorImpl<>(
-                (Supplier<List<Pair<Integer, T>>>) ArrayList::new,
-                (pairs, cur) -> {
-                    pairs.add(new Pair<>(null, cur));
-
-                    for (Pair<Integer, T> p1 : pairs) {
-                        // Initializes the first rank as it is temporary.
-                        p1.setLeft(1);
-
-                        for (Pair<Integer, T> p2 : pairs) {
-                            int comparison = comparator.compare(p1.getRight(), p2.getRight());
-                            if (comparison > 0) {
-                                p1.setLeft(p1.getLeft() + 1);
+    /**
+     * Returns a collector which compares property of each element with extractor and sorts by their rank.
+     *
+     * @param propertyExtractor  extractor to get property of the element
+     * @param propertyComparator comparator to the property for ranking
+     * @param <T>                type of input element
+     * @param <R>                type of extracted property from the element
+     * @return ranking map
+     * @see <a href="https://github.com/mtorchiano/JavaUtilities">it.polito.utility.stream.Ranking</a>
+     */
+    public static <T, R> Collector<T, ?, SortedMap<Integer, List<T>>> ranking(
+            Function<T, R> propertyExtractor, Comparator<R> propertyComparator) {
+        return collectingAndThen(
+                groupingBy(propertyExtractor, () -> new TreeMap<>(propertyComparator), toList()),
+                map -> map.entrySet().stream().collect(
+                        () -> new TreeMap<Integer, List<T>>() {
+                            @Override
+                            public String toString() {
+                                return this.entrySet().stream()
+                                        .flatMap(e -> e.getValue().stream().map(v -> e.getKey().toString() + " - " + v.toString() + " (" + propertyExtractor.apply(v) + ")"))
+                                        .collect(joining("\n"));
                             }
+                        },
+                        (ranks, entry) -> {
+                            if (ranks.isEmpty()) {
+                                ranks.put(1, entry.getValue());
+                                return;
+                            }
+
+                            Integer lastRank = ranks.lastKey();
+                            List<T> items = ranks.get(lastRank);
+                            ranks.put(lastRank + items.size(), entry.getValue());
+                        },
+                        (left, right) -> {
+                            int lastRank = left.lastKey();
+                            List<T> leftItems = left.get(lastRank);
+
+                            Integer firstKey = right.firstKey();
+                            List<T> rightItems = right.get(firstKey);
+
+                            R leftProp = propertyExtractor.apply(leftItems.get(0));
+                            R rightProp = propertyExtractor.apply(rightItems.get(0));
+
+                            if (leftProp == rightProp) {
+                                leftItems.addAll(rightItems);
+                                right.remove(firstKey);
+                            }
+
+                            int offset = lastRank + leftItems.size() - 1;
+                            right.forEach((rank, items) -> left.put(offset + rank, items));
                         }
-                    }
-                },
-                (left, right) -> {
-                    left.addAll(right);
-                    return left;
-                },
-                pairs -> {
-                    List<Entry<Integer, T>> ranks = new ArrayList<>(pairs.size());
-                    for (Pair<Integer, T> pair : pairs) {
-                        ranks.add(new SimpleEntry<>(pair.left, pair.right));
-                    }
-
-                    // Sorts by rank.
-                    ranks.sort(Entry.comparingByKey());
-
-                    return ranks;
-                },
-                Collections.singleton(Characteristics.UNORDERED)
+                )
         );
-    }
-
-    public static <T> Collector<T, ?, List<Entry<Integer, T>>> denseRanking(Comparator<? super T> comparator) {
-        return new CollectorImpl<T, List<T>, List<Entry<Integer, T>>>(
-                ArrayList::new,
-                List::add,
-                (left, right) -> {
-                    left.addAll(right);
-                    return left;
-                },
-                list -> {
-                    List<Integer> ranks = new ArrayList<>(list.size());
-                    ranks.add(1);
-
-//                    ranks.add(new Pair<>(1, list.get(0)));
-
-                    for (int i = 1; i < list.size(); i++) {
-                        T t = list.get(i);
-                        int comparison = comparator.compare(t, list.get(i - 1));
-
-                        int rank = comparison == 0 ? ranks.get(i - 1) : i + 1;
-                        ranks.add(rank);
-                    }
-
-                    List<Entry<Integer, T>> entries = new ArrayList<>(list.size());
-                    for (int i = 0; i < list.size(); i++) {
-                        entries.add(new SimpleEntry<>(ranks.get(i), list.get(i)));
-                    }
-
-                    return entries;
-                },
-                Collections.singleton(Characteristics.UNORDERED)
-        );
-    }
-
-    // -------------------------------------------------------------------------------------------------
-
-    private static class Pair<L, R> {
-        L left;
-        R right;
-
-        private Pair(L left, R right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        public L getLeft() {
-            return left;
-        }
-
-        public void setLeft(L left) {
-            this.left = left;
-        }
-
-        public R getRight() {
-            return right;
-        }
-
-        public void setRight(R right) {
-            this.right = right;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.left, this.right);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Pair<?, ?> pair = (Pair<?, ?>) o;
-            return Objects.equals(this.left, pair.left) && Objects.equals(this.right, pair.right);
-        }
-
-        @Override
-        public String toString() {
-            return "Pair(" + this.left + ", " + this.right + ")";
-        }
-    }
-
-    // -------------------------------------------------------------------------------------------------
-
-    private static class CollectorImpl<T, A, R> implements Collector<T, A, R> {
-        private final Supplier<A> supplier;
-        private final BiConsumer<A, T> accumulator;
-        private final BinaryOperator<A> combiner;
-        private final Function<A, R> finisher;
-        private final Set<Characteristics> characteristics;
-
-        private CollectorImpl(Supplier<A> supplier,
-                              BiConsumer<A, T> accumulator,
-                              BinaryOperator<A> combiner,
-                              Function<A, R> finisher,
-                              Set<Characteristics> characteristics) {
-            this.supplier = supplier;
-            this.accumulator = accumulator;
-            this.combiner = combiner;
-            this.finisher = finisher;
-            this.characteristics = characteristics;
-        }
-
-        @SuppressWarnings("unchecked")
-        private CollectorImpl(Supplier<A> supplier,
-                              BiConsumer<A, T> accumulator,
-                              BinaryOperator<A> combiner,
-                              Set<Characteristics> characteristics) {
-            this(supplier, accumulator, combiner, t -> (R) t, characteristics);
-        }
-
-        @Override
-        public BiConsumer<A, T> accumulator() {
-            return accumulator;
-        }
-
-        @Override
-        public Supplier<A> supplier() {
-            return supplier;
-        }
-
-        @Override
-        public BinaryOperator<A> combiner() {
-            return combiner;
-        }
-
-        @Override
-        public Function<A, R> finisher() {
-            return finisher;
-        }
-
-        @Override
-        public Set<Characteristics> characteristics() {
-            return characteristics;
-        }
     }
 
 }
--- src/test/groovy/io/github/imsejin/common/util/CollectorUtilsSpec.groovy
+++ src/test/groovy/io/github/imsejin/common/util/CollectorUtilsSpec.groovy
@@ -1,11 +1,13 @@
 package io.github.imsejin.common.util
 
+import io.github.imsejin.common.tool.RandomString
 import io.github.imsejin.common.tool.Stopwatch
 import spock.lang.Specification
 import spock.lang.Timeout
 
 import java.util.Map.Entry
 import java.util.concurrent.TimeUnit
+import java.util.function.Function
 
 import static io.github.imsejin.common.util.CollectorUtils.ranking
 import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.B
@@ -22,82 +24,83 @@ import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.O
 import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.S
 import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.T
 import static io.github.imsejin.common.util.CollectorUtilsSpec.Alphabet.U
-import static java.util.Comparator.comparing
-import static java.util.Comparator.comparingInt
 
 class CollectorUtilsSpec extends Specification {
 
     @SuppressWarnings("GroovyAssignabilityCheck")
     def "Collects by rank"() {
         when:
-        def ranks = values.stream().collect(ranking())
-        def rankMap = toMap(ranks)
+        def rankMap = values.stream().collect(ranking())
 
-        then:
-        values.size() >= rankMap.size()
+        then: "It can't have more keys than the number of origin values."
+        rankMap.size() <= values.size()
+
+        then: "Is it equal to the expected?"
         rankMap == expected
+
+        then: "Is it sorted by rank?"
+        def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
+        keys == keys.sort(Integer::compare)
 
         where:
         values                                                          | expected
-        [true, false, false, true]                                      | [1: false, 3: true]
-        [4, 3, 2, 2, 1, 1, 2]                                           | [1: 1, 3: 2, 6: 3, 7: 4]
-        [3.14, -1.173, 0.1, -0.2, 0.0]                                  | [1: -1.173, 2: -0.2, 3: 0.0, 4: 0.1, 5: 3.14]
-        [16, 0, -8, 1024, -128, 64, 256, -512, 32]                      | [1: -512, 2: -128, 3: -8, 4: 0, 5: 16, 6: 32, 7: 64, 8: 256, 9: 1024]
-        ['A', 'b', '9', '0', 'D', 'e', '5'] as List<Character>          | [1: '0', 2: '5', 3: '9', 4: 'A', 5: 'D', 6: 'b', 7: 'e'] as Map<Integer, Character>
-        ["beta", "b", "alpha", "a", "gamma", "g", ""]                   | [1: "", 2: "a", 3: "alpha", 4: "b", 5: "beta", 6: "g", 7: "gamma"]
-        [I, O, G, I, T, H, U, B, I, M, S, E, J, I, N, C, O, M, M, O, N] | [1: B, 2: C, 3: E, 4: G, 5: H, 6: I, 10: J, 11: M, 14: N, 16: O, 19: S, 20: T, 21: U]
+        [true, false, false, true]                                      | [1: [false, false], 3: [true, true]]
+        [4, 3, 2, 2, 1, 1, 2]                                           | [1: [1, 1], 3: [2, 2, 2], 6: [3], 7: [4]]
+        [3.14, -1.173, 0.1, -0.2, 0.0]                                  | [1: [-1.173], 2: [-0.2], 3: [0.0], 4: [0.1], 5: [3.14]]
+        [16, 0, -8, 1024, -128, 64, 256, -512, 32]                      | [1: [-512], 2: [-128], 3: [-8], 4: [0], 5: [16], 6: [32], 7: [64], 8: [256], 9: [1024]]
+        ['A', 'b', '9', '0', 'D', 'e', '5'] as List<Character>          | [1: ['0'], 2: ['5'], 3: ['9'], 4: ['A'], 5: ['D'], 6: ['b'], 7: ['e']] as Map<Integer, Character>
+        ["beta", "b", "alpha", "a", "gamma", "g", ""]                   | [1: [""], 2: ["a"], 3: ["alpha"], 4: ["b"], 5: ["beta"], 6: ["g"], 7: ["gamma"]]
+        [I, O, G, I, T, H, U, B, I, M, S, E, J, I, N, C, O, M, M, O, N] | [1: [B], 2: [C], 3: [E], 4: [G], 5: [H], 6: [I, I, I, I], 10: [J], 11: [M, M, M], 14: [N, N], 16: [O, O, O], 19: [S], 20: [T], 21: [U]]
     }
 
     def "Collects by rank with comparator"() {
         when:
-        def ranks = values.stream().collect(ranking(comparator as Comparator<? super Object>))
-        def rankMap = toMap(ranks)
+        def rankMap = values.stream().collect(ranking(extractor as Function, comparator as Comparator)) as SortedMap<Integer, ?>
 
-        then:
-        values.size() >= rankMap.size()
+        then: "It can't have more keys than the number of origin values."
+        rankMap.size() <= values.size()
+
+        then: "Is it equal to the expected?"
         rankMap == expected
 
+        then: "Is it sorted by rank?"
+        def keys = rankMap.entrySet().collect(Entry::getKey) as List<Integer>
+        keys == keys.sort(Integer::compare)
+
         where:
-        values                                        | comparator                                                                  || expected
-        [true, false, false, true]                    | comparingInt({ it.toString().length() })                                    || [1: true, 3: false]
-        [16, 0, -8, 1024, -128, 64, 256, -512, 32]    | Integer::compare                                                            || [1: -512, 2: -128, 3: -8, 4: 0, 5: 16, 6: 32, 7: 64, 8: 256, 9: 1024]
-        ["beta", "b", "alpha", "a", "gamma", "g", ""] | String::compareTo                                                           || [1: "", 2: "a", 3: "alpha", 4: "b", 5: "beta", 6: "g", 7: "gamma"]
-        ["beta", "b", "alpha", "a", "gamma", "g", ""] | comparingInt(String::length).thenComparing(String::compareTo as Comparator) || [1: "", 2: "a", 3: "b", 4: "g", 5: "beta", 6: "alpha", 7: "gamma"]
-        [C, O, M, M, O, N, U, T, I, L, I, T, I, E, S] | comparing(Enum::ordinal)                                                    || [1: C, 2: E, 3: I, 6: L, 7: M, 9: N, 10: O, 12: S, 13: T, 15: U]
+        values                                        | extractor                  | comparator        || expected
+        [true, false, false, true]                    | { it.toString().length() } | Integer::compare  || [1: [true, true], 3: [false, false]]
+        [16, 0, -8, 1024, -128, 64, 256, -512, 32]    | Function.identity()        | Integer::compare  || [1: [-512], 2: [-128], 3: [-8], 4: [0], 5: [16], 6: [32], 7: [64], 8: [256], 9: [1024]]
+        ["beta", "b", "alpha", "a", "gamma", "g", ""] | Function.identity()        | String::compareTo || [1: [""], 2: ["a"], 3: ["alpha"], 4: ["b"], 5: ["beta"], 6: ["g"], 7: ["gamma"]]
+        ["beta", "b", "alpha", "a", "gamma", "g", ""] | String::length             | Integer::compare  || [1: [""], 2: ["b", "a", "g"], 5: ["beta"], 6: ["alpha", "gamma"]]
+        [C, O, M, M, O, N, U, T, I, L, I, T, I, E, S] | Enum::ordinal              | Integer::compare  || [1: [C], 2: [E], 3: [I, I, I], 6: [L], 7: [M, M], 9: [N], 10: [O, O], 12: [S], 13: [T, T], 15: [U]]
     }
 
-    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    @Timeout(value = 10, unit = TimeUnit.SECONDS)
     def "Measures performance"() {
         def stopwatch = new Stopwatch(TimeUnit.MILLISECONDS)
-        stopwatch.start("Creates 1000 random values")
+        stopwatch.start("Creates %,d random values", 10_000_000)
 
         given:
-        def values = (0..1000).collect { UUID.randomUUID() }
+        def randomString = new RandomString()
+        def values = (1..10_000_000).collect { randomString.nextString(2) }
         stopwatch.stop()
 
         when:
         stopwatch.start("Collects them by rank")
-        def ranks = values.stream().collect(ranking())
+        def rankMap = values.stream().collect(ranking())
         stopwatch.stop()
         println stopwatch.statistics
 
         then:
-        def rankMap = toMap(ranks)
-        values.size() >= rankMap.size()
+        !rankMap.isEmpty()
+        rankMap.size() <= values.size()
     }
 
     // -------------------------------------------------------------------------------------------------
 
     enum Alphabet {
         A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
-    }
-
-    static <T> Map<Integer, T> toMap(Iterable<Entry<Integer, T>> entries) {
-        Map<Integer, T> map = [:]
-        entries.each {
-            map[it.key] = it.value
-        }
-        map
     }
 
 }
